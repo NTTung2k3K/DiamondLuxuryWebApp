b9d849eaee1379563f5c60cc34d5f7cd7e4a471f
LilMIKHuwng <hunglqse171422@fpt.edu.vn>
2024-05-22 15:45:26 +0700

    Anout5

--- DiamondLuxurySolution/DiamondLuxurySolution.Data/obj/Debug/net8.0/DiamondLuxurySolution.Data.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.Data/obj/Debug/net8.0/DiamondLuxurySolution.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8286f667cdbe236e85c873c7d7632e3d4387f469")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f70db67318b235b582b46208b0f2c635aa52b7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DiamondLuxurySolution/DiamondLuxurySolution.Utilities/obj/Debug/net8.0/DiamondLuxurySolution.Utilities.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.Utilities/obj/Debug/net8.0/DiamondLuxurySolution.Utilities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.Utilities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8286f667cdbe236e85c873c7d7632e3d4387f469")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f70db67318b235b582b46208b0f2c635aa52b7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.Utilities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.Utilities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
