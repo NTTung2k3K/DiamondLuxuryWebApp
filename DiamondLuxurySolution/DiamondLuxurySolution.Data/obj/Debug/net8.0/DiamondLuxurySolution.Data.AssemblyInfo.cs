9e74033cec2c598dbb91d78545de6b11fd344967
Nguyen Thanh Tung <TungNTSE171746@fpt.edu.vn>
2024-05-24 15:02:11 +0700

    fix tai thang lam

--- DiamondLuxurySolution/DiamondLuxurySolution.Data/obj/Debug/net8.0/DiamondLuxurySolution.Data.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.Data/obj/Debug/net8.0/DiamondLuxurySolution.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecc867632de9e16e591651fcc6da0b4cce827d7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43e8e58c05d371fbfb608c8573919ba7a68e69af")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
