31c78c7bef36c02598c23381c91ff6158a5aceb1
Nguyen Thanh Tung <TungNTSE171746@fpt.edu.vn>
2024-05-24 15:25:02 +0700

    ready to merge

--- DiamondLuxurySolution/DiamondLuxurySolution.BackendApi/obj/Debug/net8.0/DiamondLuxurySolution.BackendApi.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.BackendApi/obj/Debug/net8.0/DiamondLuxurySolution.BackendApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.BackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecc867632de9e16e591651fcc6da0b4cce827d7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e18b546081641b442b7cd9bf41f96fed3f2f4e7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.BackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.BackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DiamondLuxurySolution/DiamondLuxurySolution.ViewModel/obj/Debug/net8.0/DiamondLuxurySolution.ViewModel.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.ViewModel/obj/Debug/net8.0/DiamondLuxurySolution.ViewModel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.ViewModel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15c81394138a577ef10286429c595b4c7030d86b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e18b546081641b442b7cd9bf41f96fed3f2f4e7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.ViewModel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.ViewModel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
