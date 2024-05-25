27b0a5527ebed665d89a2375e1a0eb31c309f3a4
quannmin <laogiado2003@gmail.com>
2024-05-25 10:41:23 +0700

    Merge/Api/GemPriceList

--- DiamondLuxurySolution/DiamondLuxurySolution.ViewModel/obj/Debug/net8.0/DiamondLuxurySolution.ViewModel.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.ViewModel/obj/Debug/net8.0/DiamondLuxurySolution.ViewModel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.ViewModel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b10f2afa16d3629278018c396c51f88c41604fbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14cb514da7b386238f214fb3f6ed6b5d512dcd17")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.ViewModel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.ViewModel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
