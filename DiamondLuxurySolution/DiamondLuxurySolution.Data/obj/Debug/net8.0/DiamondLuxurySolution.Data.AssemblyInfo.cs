255b38405ee7ee41429950a743a37d78a1af50a5
Nguyen Thanh Tung <TungNTSE171746@fpt.edu.vn>
2024-05-27 16:39:29 +0700

    Merge

--- DiamondLuxurySolution/DiamondLuxurySolution.Data/obj/Debug/net8.0/DiamondLuxurySolution.Data.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.Data/obj/Debug/net8.0/DiamondLuxurySolution.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c56e6ea5b41c3d50dda794ad034eb84f9979761")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5d2e9a14a1b79843026f219af397009607a4148")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
