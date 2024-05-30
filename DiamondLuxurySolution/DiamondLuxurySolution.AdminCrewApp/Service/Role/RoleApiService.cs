b1ee3c98d81a8c7ae6d18f23d1d44ba5add99c90
Nguyen Thanh Tung <TungNTSE171746@fpt.edu.vn>
2024-05-30 15:02:25 +0700

    Done Role MVC

---
+++ DiamondLuxurySolution/DiamondLuxurySolution.AdminCrewApp/Service/Role/RoleApiService.cs
@@ -0,0 +1,50 @@
+﻿using Azure.Core;
+using DiamondLuxurySolution.AdminCrewApp.Services;
+using DiamondLuxurySolution.ViewModel.Common;
+using DiamondLuxurySolution.ViewModel.Models.Role;
+
+namespace DiamondLuxurySolution.AdminCrewApp.Service.Role
+{
+    public class RoleApiService : BaseApiService, IRoleApiService
+    {
+        public RoleApiService(IHttpClientFactory httpClientFactory, IConfiguration configuration, IHttpContextAccessor httpContextAccessor) : base(httpClientFactory, configuration, httpContextAccessor)
+        {
+        }
+
+        public async Task<ApiResult<bool>> CreateRole(CreateRoleRequest request)
+        {
+            var data = await PostAsync<bool>("api/Roles/Create", request);
+            return data;
+        }
+
+        public async Task<ApiResult<bool>> DeleteRole(DeleteRoleRequest request)
+        {
+            var data = await DeleteAsync<bool>("api/Roles/Delete?roleId="+request.RoleId);
+            return data;
+        }
+
+        public async Task<ApiResult<RoleVm>> GetRoleById(Guid RoleId)
+        {
+            var data = await GetAsync<RoleVm>("api/Roles/GetById?RoleId="+RoleId);
+            return data;
+        }
+
+        public async Task<ApiResult<PageResult<RoleVm>>> GetRolePagination(ViewRoleRequest request)
+        {
+            var data = await GetAsync<PageResult<RoleVm>>($"api/Roles/View?Keyword={request.Keyword}&pageIndex={request.pageIndex}");
+            return data;
+        }
+
+        public async Task<ApiResult<List<RoleVm>>> GetRolesForView()
+        {
+            var data = await GetAsync<List<RoleVm>>("api/Roles/GetAllRole");
+            return data;
+        }
+
+        public async Task<ApiResult<bool>> UpdateRole(UpdateRoleRequest request)
+        {
+            var data = await PutAsync<bool>("api/Roles/Update", request);
+            return data;
+        }
+    }
+}
---
+++ DiamondLuxurySolution/DiamondLuxurySolution.Application/Repository/Role/RoleInitializer.cs
@@ -0,0 +1,75 @@
+﻿using DiamondLuxurySolution.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiamondLuxurySolution.Application.Repository.Role
+{
+    public class RoleInitializer : IRoleInitializer
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
+        public RoleInitializer(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task<bool> CreateDefaultRole()
+        {
+            var AdminRoleName = DiamondLuxurySolution.Utilities.Constants.Systemconstant.UserRoleDefault.Admin.ToString();
+            var CustomerRoleName = DiamondLuxurySolution.Utilities.Constants.Systemconstant.UserRoleDefault.Customer.ToString();
+            var DeliveryStaffRoleName = DiamondLuxurySolution.Utilities.Constants.Systemconstant.UserRoleDefault.DeliveryStaff.ToString();
+            var ManagerRoleName = DiamondLuxurySolution.Utilities.Constants.Systemconstant.UserRoleDefault.Manager.ToString();
+            var SalesStaffRoleName = DiamondLuxurySolution.Utilities.Constants.Systemconstant.UserRoleDefault.SalesStaff.ToString();
+
+            var defaultRoleNameList = new List<string>();
+            defaultRoleNameList.Add(AdminRoleName);
+            defaultRoleNameList.Add(CustomerRoleName);
+            defaultRoleNameList.Add(DeliveryStaffRoleName);
+            defaultRoleNameList.Add(ManagerRoleName);
+            defaultRoleNameList.Add(SalesStaffRoleName);
+
+            foreach (var roleName in defaultRoleNameList)
+            {
+                var role = await _roleManager.FindByNameAsync(roleName);
+                if (role != null)
+                {
+                    continue;
+                }
+                var roleAdd = new AppRole()
+                {
+                    Name = roleName,
+                    Description = roleName
+                };
+                var status = await _roleManager.CreateAsync(roleAdd);
+            }
+            return true;
+        }
+
+        public async Task<bool> CreateAdminAccount()
+        {
+            var user = new AppUser()
+            {
+                Fullname = "Admin1@",
+                Email = "Admin1@gmail.com",
+                Dob = DateTime.Now,
+                PhoneNumber = "0999999999",
+                UserName = "Admin1@",
+                Status = DiamondLuxurySolution.Utilities.Constants.Systemconstant.StaffStatus.Active.ToString(),
+            };
+
+            var status = await _userManager.CreateAsync(user, "Admin1@");
+
+            var roleFindByName = await _roleManager.FindByNameAsync(DiamondLuxurySolution.Utilities.Constants.Systemconstant.UserRoleDefault.Admin);
+            await _userManager.AddToRoleAsync(user, roleFindByName.Name);
+            _userManager.UpdateAsync(user);
+
+            return true;
+        }
+    }
+
+}
