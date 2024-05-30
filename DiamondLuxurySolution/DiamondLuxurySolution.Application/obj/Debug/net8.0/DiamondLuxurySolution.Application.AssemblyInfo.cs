0cda553ef88aba4213a5f239c298fb8eef060e72
Thang Lam <thanglam271003@gmail.com>
2024-05-30 22:13:47 +0700

    complete by ThangL

--- DiamondLuxurySolution/DiamondLuxurySolution.Application/obj/Debug/net8.0/DiamondLuxurySolution.Application.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.Application/obj/Debug/net8.0/DiamondLuxurySolution.Application.AssemblyInfo.cs
@@ -14,15 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24296e4f40327c1606cdbaf20a70faa262d757d0")]
-=======
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeffecaad468c2f61694135675a35cb150fbe483")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26a196123a17dbfe3ab483f26ff2bc2a18050949")]
->>>>>>> 953636842e7bfe0c8626a339d1306a738b20037e
->>>>>>> c9e6e6aecd389dfaa5f320bedb9e8090e6d19fb0
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+425989fa9225e4471c42e74529814f805583baaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
