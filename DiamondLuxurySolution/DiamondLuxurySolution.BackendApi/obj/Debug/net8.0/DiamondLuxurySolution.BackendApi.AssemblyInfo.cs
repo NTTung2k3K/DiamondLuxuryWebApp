e340d7b4c762cae9aefd25d1c671d7c5380d56ed
quannmin <laogiado2003@gmail.com>
2024-05-30 19:56:37 +0700

    Feature/Mvc/InspectionCertificate

--- DiamondLuxurySolution/DiamondLuxurySolution.BackendApi/obj/Debug/net8.0/DiamondLuxurySolution.BackendApi.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.BackendApi/obj/Debug/net8.0/DiamondLuxurySolution.BackendApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.BackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32df3baf9d5630be980198db59565a40089f27b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeffecaad468c2f61694135675a35cb150fbe483")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.BackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.BackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DiamondLuxurySolution/DiamondLuxurySolution.ViewModel/Models/InspectionCertificate/UpdateInspectionCertificateRequest.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.ViewModel/Models/InspectionCertificate/UpdateInspectionCertificateRequest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace DiamondLuxurySolution.ViewModel.Models.InspectionCertificate
     {
         public string InspectionCertificateId { get; set; } = null!;
 
+        [Required(ErrorMessage = "Yêu cầu tên của giấy chứng nhận")]
         public string? InspectionCertificateName { get; set; }
 
         public DateTime? DateGrading { get; set; }
