953636842e7bfe0c8626a339d1306a738b20037e
LilMIKHuwng <hunglqse171422@fpt.edu.vn>
2024-05-30 15:55:49 +0700

    Complete by Hung

--- DiamondLuxurySolution/DiamondLuxurySolution.Utilities/obj/Debug/net8.0/DiamondLuxurySolution.Utilities.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.Utilities/obj/Debug/net8.0/DiamondLuxurySolution.Utilities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.Utilities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeffecaad468c2f61694135675a35cb150fbe483")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26a196123a17dbfe3ab483f26ff2bc2a18050949")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.Utilities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.Utilities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
