95790222ccd483d70a412ab23701ec46fd0d6df4
quannmin <laogiado2003@gmail.com>
2024-05-31 15:08:41 +0700

    Fix-Complete

--- DiamondLuxurySolution/DiamondLuxurySolution.AdminCrewApp/Program.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.AdminCrewApp/Program.cs
@@ -1,19 +1,13 @@
-<<<<<<< HEAD
-=======
 
 using DiamondLuxurySolution.AdminCrewApp.Service.Category;
->>>>>>> 5a3d0a77f6492a0e0a6e38f5df87b23f73c1b6b1
 using DiamondLuxurySolution.AdminCrewApp.Service.IInspectionCertificate;
 using DiamondLuxurySolution.AdminCrewApp.Service.Contact;
 using DiamondLuxurySolution.AdminCrewApp.Service.Platform;
 using DiamondLuxurySolution.AdminCrewApp.Service.Role;
 using DiamondLuxurySolution.Data.EF;
 using Microsoft.EntityFrameworkCore;
-<<<<<<< HEAD
 using DiamondLuxurySolution.AdminCrewApp.Service.Material;
-=======
 using DiamondLuxurySolution.AdminCrewApp.Service.Slide;
->>>>>>> 5a3d0a77f6492a0e0a6e38f5df87b23f73c1b6b1
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,12 +17,11 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<IRoleApiService, RoleApiService>();
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddTransient<IPlatformApiService, PlatformApiService>();
-<<<<<<< HEAD
 builder.Services.AddTransient<IInspectionCertificateApiService, InspectionCertificateApiService>();
 builder.Services.AddTransient<IContactApiService, ContactApiService>();
 builder.Services.AddTransient<IMaterialApiService, MaterialApiService>();
 
-=======
+
 
 
 builder.Services.AddTransient<ISlideApiService, SlideApiService>();
@@ -37,7 +30,6 @@ builder.Services.AddTransient<ICategoryApiService, CategoryApiService>();
 builder.Services.AddTransient<IInspectionCertificateApiService, InspectionCertificateApiService>();
 
 builder.Services.AddTransient<IContactApiService, ContactApiService>();
->>>>>>> 5a3d0a77f6492a0e0a6e38f5df87b23f73c1b6b1
 builder.Services.AddDbContext<LuxuryDiamondShopContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("eShopSolutionDb"));
