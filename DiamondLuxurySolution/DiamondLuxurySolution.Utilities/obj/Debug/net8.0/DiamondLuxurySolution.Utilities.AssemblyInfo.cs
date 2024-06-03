116ac3d3927b1652f18a50f8e47b207db5335a73
LilMIKHuwng <hunglqse171422@fpt.edu.vn>
2024-06-03 19:24:59 +0700

    Complete By Hung

--- DiamondLuxurySolution/DiamondLuxurySolution.Utilities/obj/Debug/net8.0/DiamondLuxurySolution.Utilities.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.Utilities/obj/Debug/net8.0/DiamondLuxurySolution.Utilities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.Utilities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b562cfe2c09dde2101ef14348da513289047a9e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+937c9758570142b147d6dd93c4824a1442a99a85")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.Utilities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.Utilities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DiamondLuxurySolution/DiamondLuxurySolution.ViewModel/obj/Debug/net8.0/DiamondLuxurySolution.ViewModel.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.ViewModel/obj/Debug/net8.0/DiamondLuxurySolution.ViewModel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.ViewModel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b562cfe2c09dde2101ef14348da513289047a9e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+937c9758570142b147d6dd93c4824a1442a99a85")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.ViewModel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.ViewModel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
