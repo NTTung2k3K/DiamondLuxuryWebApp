54c6ec82b3d5d3c8834f6473107debe29d818fd3
LilMIKHuwng <hunglqse171422@fpt.edu.vn>
2024-06-14 12:29:15 +0700

    Fix ProductDetail

--- DiamondLuxurySolution/DiamondLuxurySolution.Data/obj/Debug/net8.0/DiamondLuxurySolution.Data.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.Data/obj/Debug/net8.0/DiamondLuxurySolution.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd91014316cff25d463a76ca109239ed4ce4aa3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfd1c9991202a3160beb9d776374f4dcbfb803ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
