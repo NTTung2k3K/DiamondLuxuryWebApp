b652c38bced1ae401eddcbcdf7ce5ebf0df60972
quannmin <laogiado2003@gmail.com>
2024-06-16 15:32:20 +0700

    TestDeliveryStaff

---
+++ DiamondLuxurySolution/DiamondLuxurySolution.ViewModel/Models/User/Staff/UpdateStatusOrderForDeliveryStaff.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiamondLuxurySolution.ViewModel.Models.User.Staff
+{
+    public class UpdateStatusOrderForDeliveryStaff
+    {
+        public string orderId { get; set; }
+    }
+}
