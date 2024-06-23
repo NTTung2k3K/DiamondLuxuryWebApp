2257b503ed643f26c0d942a426222cbd581d5a36
Thang Lam <thanglam271003@gmail.com>
2024-06-24 00:22:44 +0700

    ok

--- DiamondLuxurySolution/DiamondLuxurySolution.WebApp/obj/Debug/net8.0/DiamondLuxurySolution.WebApp.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.WebApp/obj/Debug/net8.0/DiamondLuxurySolution.WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62be2ff3024c0565bee16b0df9fdf1478bc12a24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fef191d0699172d30f887f62c2806aa082c6be63")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
