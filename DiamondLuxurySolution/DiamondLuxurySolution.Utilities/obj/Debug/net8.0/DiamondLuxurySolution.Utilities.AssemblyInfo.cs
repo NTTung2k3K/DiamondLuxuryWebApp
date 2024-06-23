fef191d0699172d30f887f62c2806aa082c6be63
Thang Lam <thanglam271003@gmail.com>
2024-06-23 22:58:19 +0700

    finish search

--- DiamondLuxurySolution/DiamondLuxurySolution.Utilities/obj/Debug/net8.0/DiamondLuxurySolution.Utilities.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.Utilities/obj/Debug/net8.0/DiamondLuxurySolution.Utilities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.Utilities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df9762ca0a6938bb9ff88420bf6e35e82a168e50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62be2ff3024c0565bee16b0df9fdf1478bc12a24")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.Utilities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.Utilities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
