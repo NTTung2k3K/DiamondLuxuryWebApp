ac4a0882baff963d937914871b5732947de935c3
Thang Lam <thanglam271003@gmail.com>
2024-06-26 17:11:12 +0700

    finish money

--- DiamondLuxurySolution/DiamondLuxurySolution.WebApp/obj/Debug/net8.0/DiamondLuxurySolution.WebApp.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.WebApp/obj/Debug/net8.0/DiamondLuxurySolution.WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76e8ad9e2203d0523453a2a8318e7463085f77ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c843ff822f992a9797e07a7e3206799de307b1f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
