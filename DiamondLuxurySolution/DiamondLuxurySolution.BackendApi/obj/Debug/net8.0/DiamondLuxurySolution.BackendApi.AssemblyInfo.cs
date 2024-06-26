1178df91e62d630e60411bcf89616921da8f3e18
Nguyen Thanh Tung <TungNTSE171746@fpt.edu.vn>
2024-06-26 12:50:13 +0700

    Done Fix bug from dev detected

--- DiamondLuxurySolution/DiamondLuxurySolution.BackendApi/obj/Debug/net8.0/DiamondLuxurySolution.BackendApi.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.BackendApi/obj/Debug/net8.0/DiamondLuxurySolution.BackendApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.BackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff2082947452bc7a8400e31662dd465bed10213d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8374bad06a032df44af45e9561aa791f7c9f5e41")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.BackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.BackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
