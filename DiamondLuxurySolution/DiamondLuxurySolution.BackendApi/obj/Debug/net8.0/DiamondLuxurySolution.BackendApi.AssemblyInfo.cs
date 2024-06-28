a3217e3a4ff710364427a38a0f41702573aabfcf
Nguyen Thanh Tung <TungNTSE171746@fpt.edu.vn>
2024-06-28 07:47:23 +0700

    done fix deposit

--- DiamondLuxurySolution/DiamondLuxurySolution.BackendApi/obj/Debug/net8.0/DiamondLuxurySolution.BackendApi.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.BackendApi/obj/Debug/net8.0/DiamondLuxurySolution.BackendApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.BackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c83fc654354793652a2a483b2000daf6b6fbb33a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5ecbb3d1b5663bf1bbfda0b8ada5d027f019199")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.BackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.BackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
