ee6d7fa2d552618794694dfe6bb56996b71e9b59
Nguyen Thanh Tung <TungNTSE171746@fpt.edu.vn>
2024-06-28 12:56:03 +0700

    gh

--- DiamondLuxurySolution/DiamondLuxurySolution.Application/obj/Debug/net8.0/DiamondLuxurySolution.Application.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.Application/obj/Debug/net8.0/DiamondLuxurySolution.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f02a50c7deed2fdad6a9a5117ba771d5f8384ea2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b8ca118a39a9a7dbddf7e1211ac7e10fd912dda")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
