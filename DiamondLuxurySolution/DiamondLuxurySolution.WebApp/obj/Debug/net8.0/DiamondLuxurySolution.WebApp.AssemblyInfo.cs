502fe7045ea27f0839c3024011585f73fa162b7d
quannmin <laogiado2003@gmail.com>
2024-06-29 15:18:11 +0700

    Home/News/Promotion

--- DiamondLuxurySolution/DiamondLuxurySolution.WebApp/obj/Debug/net8.0/DiamondLuxurySolution.WebApp.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.WebApp/obj/Debug/net8.0/DiamondLuxurySolution.WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d7d497d42813a6d5f7272db085a1d3bd90cb004")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d78a22967a21a36e54c208085d3989e72d7f0903")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
