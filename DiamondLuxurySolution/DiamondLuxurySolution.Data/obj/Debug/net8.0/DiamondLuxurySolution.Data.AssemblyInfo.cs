0f8691e64ada5dc76499263914abbf98673b3437
LilMIKHuwng <hunglqse171422@fpt.edu.vn>
2024-07-06 00:30:19 +0700

    Coming

--- DiamondLuxurySolution/DiamondLuxurySolution.Data/obj/Debug/net8.0/DiamondLuxurySolution.Data.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.Data/obj/Debug/net8.0/DiamondLuxurySolution.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+462cf38284c65a408abbf293489610f2221fe93e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dae3ca7cfc1953eae9b97afd80e8ca53f4dc2eff")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
