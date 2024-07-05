8d0fa45b8340459fbee9bc033bc31e5fb13f6921
Nguyen Thanh Tung <TungNTSE171746@fpt.edu.vn>
2024-07-05 14:29:19 +0700

    fix con kip

--- DiamondLuxurySolution/DiamondLuxurySolution.ViewModel/obj/Debug/net8.0/DiamondLuxurySolution.ViewModel.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.ViewModel/obj/Debug/net8.0/DiamondLuxurySolution.ViewModel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.ViewModel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba70502b6a7756f78b0b2add3ac357634cf0a2fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a6a99a451c0b46313806fa23589e88fb43a288f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.ViewModel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.ViewModel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
