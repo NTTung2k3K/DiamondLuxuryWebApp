99b28058d36f3fcfe32b3528805d02206fba004e
Thang Lam <thanglam271003@gmail.com>
2024-07-07 01:37:39 +0700

    finish search

--- DiamondLuxurySolution/DiamondLuxurySolution.ViewModel/obj/Debug/net8.0/DiamondLuxurySolution.ViewModel.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.ViewModel/obj/Debug/net8.0/DiamondLuxurySolution.ViewModel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.ViewModel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26f7d7dcadf44bc9b65efa2a3d2afc3f7c8a0ef2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9d4417052e6eb2f9a5fc07b856b874ca381990e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.ViewModel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.ViewModel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DiamondLuxurySolution/DiamondLuxurySolution.WebApp/Service/Search/ISearchProductApiService.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.WebApp/Service/Search/ISearchProductApiService.cs
@@ -6,5 +6,6 @@ namespace DiamondLuxurySolution.WebApp.Service.Product
     public interface ISearchProductApiService
     {
         public Task<ApiResult<PageResult<ProductVm>>> ViewProduct(ViewProductRequest request);
+        public Task<ApiResult<List<ProductVm>>> GetAll();
     }
 }
