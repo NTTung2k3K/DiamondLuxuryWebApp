7feabb7a4cfef4e0c564df2f64f35064423f1bf6
Nguyen Thanh Tung <TungNTSE171746@fpt.edu.vn>
2024-07-06 18:39:51 +0700

    pull main

--- DiamondLuxurySolution/DiamondLuxurySolution.ViewModel/obj/Debug/net8.0/DiamondLuxurySolution.ViewModel.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.ViewModel/obj/Debug/net8.0/DiamondLuxurySolution.ViewModel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.ViewModel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d89849e666e526c7ebf0f1263263ddfe4d9165a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+462cf38284c65a408abbf293489610f2221fe93e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.ViewModel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.ViewModel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
