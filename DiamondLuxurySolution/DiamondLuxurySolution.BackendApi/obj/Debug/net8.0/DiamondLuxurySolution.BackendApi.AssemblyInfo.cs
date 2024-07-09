12d0275a984d4af813b5f9270b778a5f7252bf12
Thang Lam <thanglam271003@gmail.com>
2024-07-09 07:39:58 +0700

    fix_lat_vat

--- DiamondLuxurySolution/DiamondLuxurySolution.BackendApi/obj/Debug/net8.0/DiamondLuxurySolution.BackendApi.AssemblyInfo.cs
+++ DiamondLuxurySolution/DiamondLuxurySolution.BackendApi/obj/Debug/net8.0/DiamondLuxurySolution.BackendApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondLuxurySolution.BackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99b28058d36f3fcfe32b3528805d02206fba004e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02ca0cdb94314a9b2b4a6a96a0316971fb137ed1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondLuxurySolution.BackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondLuxurySolution.BackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
